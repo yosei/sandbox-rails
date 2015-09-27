c91122c5840d1113b021f339c319d44e8133a7aa
Yosei Ito <yousei.itou@gmail.com>
2015-09-27 14:27:43 +0900

    Reverse the order of snapshots.

--- app/controllers/webcam_controller.rb
+++ app/controllers/webcam_controller.rb
@@ -7,7 +7,7 @@ class WebcamController < ApplicationController
     st = (DateTime.now - 1.days).strftime("%F %T")
     en = DateTime.now.strftime("%F %T")
     # Get stat.
-    sql = "SELECT ts,n,(SELECT ts FROM webcam_logs WHERE id = max_marks_id) AS mts FROM webcam_logs_dailies WHERE webcam_id = '#{id}'"
+    sql = "SELECT ts,n,(SELECT ts FROM webcam_logs WHERE id = max_marks_id) AS mts FROM webcam_logs_dailies WHERE webcam_id = '#{id}' ORDER BY ts DESC"
     @data = []
     results = db.execute(sql)
     results.each do |row|
@@ -20,7 +20,7 @@ class WebcamController < ApplicationController
     id = params[:id]
     date = params[:date]
     db = ActiveRecord::Base.connection
-    sql = "SELECT ts,marks,memo FROM webcam_logs WHERE webcam_id = '#{id}' AND date(ts) = '#{date}'"
+    sql = "SELECT ts,marks,memo FROM webcam_logs WHERE webcam_id = '#{id}' AND date(ts) = '#{date}' ORDER BY ts DESC"
     @data = []
     results = db.execute(sql)
     results.each do |row|
